d6a4fc6b060ecdadf8209e1541d7cf4b58b4f7b2
RedeemerNtumy <ntumyredeemer@gmail.com>
2024-09-27 22:24:23 +0000

    Local Test Utilities commented for environment switching

--- src/test/java/utils/TestUtilities.java
+++ src/test/java/utils/TestUtilities.java
@@ -18,3 +18,25 @@ public class TestUtilities {
         }
     }
 }
+
+
+//package utils;
+//
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.remote.DesiredCapabilities;
+//import org.openqa.selenium.remote.RemoteWebDriver;
+//import java.net.URL;
+//
+//public class TestUtilities {
+//
+//    public static WebDriver setupDriver() {
+//        try {
+//            DesiredCapabilities capabilities = new DesiredCapabilities();
+//            capabilities.setCapability("browserName", "MicrosoftEdge");
+//            WebDriver driver = new RemoteWebDriver(new URL(System.getenv("SELENIUM_REMOTE_URL")), capabilities);
+//            return driver;
+//        } catch (Exception e) {
+//            throw new RuntimeException("Failed to connect to Selenium Hub: " + e.getMessage());
+//        }
+//    }
+//}
