121993a1474fe7ab2c36ff27f189317764356cd0
RedeemerNtumy <ntumyredeemer@gmail.com>
2024-09-27 22:22:14 +0000

    Currently building.Even though tests are identified, it cannnot run them as it is facing some errors

--- src/test/java/utils/TestUtilities.java
+++ src/test/java/utils/TestUtilities.java
@@ -1,12 +1,20 @@
 package utils;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.URL;
 
 public class TestUtilities {
 
     public static WebDriver setupDriver() {
-        System.setProperty("webdriver.edge.driver", "resources/msedgedriver");
-        return new EdgeDriver();
+        try {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("browserName", "MicrosoftEdge");
+            WebDriver driver = new RemoteWebDriver(new URL(System.getenv("SELENIUM_REMOTE_URL")), capabilities);
+            return driver;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to connect to Selenium Hub: " + e.getMessage());
+        }
     }
 }
