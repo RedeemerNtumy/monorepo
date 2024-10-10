e853e98f12afe2a6ffcf837ab7cbcf29584d1285
RedeemerNtumy <ntumyredeemer@gmail.com>
2024-10-10 19:13:00 +0000

    eureka solved

--- skillspace-backend/User-Service/src/main/java/com/dennis/user_service/UserServiceApplication.java
+++ skillspace-backend/User-Service/src/main/java/com/dennis/user_service/UserServiceApplication.java
@@ -1,5 +1,6 @@
 package com.dennis.user_service;
 
+import com.netflix.discovery.EurekaNamespace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
